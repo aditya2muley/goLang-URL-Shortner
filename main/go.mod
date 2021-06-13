d304433628dce34834e50f85762798ed6b4894f6
aditya2muley <53964800+aditya2muley@users.noreply.github.com>
2021-06-13 16:22:51 +0530

    added map handler functionality

---
+++ main/go.mod
@@ -0,0 +1,7 @@
+module example.com/main
+
+go 1.16
+
+replace example.com/urlshort => ../urlshort
+
+require example.com/urlshort v0.0.0-00010101000000-000000000000
--- main/main.go
+++ main/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"example.com/urlshort"
 )
 
 func main() {
@@ -10,25 +12,26 @@ func main() {
 
 	// Build the MapHandler using the mux as the fallback
 	pathsToUrls := map[string]string{
-		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
-		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+		"/google": "https://google.com",
+		"/yahoo":  "https://yahoo.com",
 	}
+
 	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
 
 	// Build the YAMLHandler using the mapHandler as the
 	// fallback
 	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
-`
+	- path: /urlshort
+	  url: https://github.com/gophercises/urlshort
+	- path: /urlshort-final
+	  url: https://github.com/gophercises/urlshort/tree/solution
+	`
 	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", yamlHandler)
+	http.ListenAndServe(":8080", mapHandler)
 }
 
 func defaultMux() *http.ServeMux {
